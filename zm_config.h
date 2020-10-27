cc70845eac214e26836deffc82e92336628f71e4
LCODVME <2685981436@qq.com>
2020-10-27 17:49:02 +0800

    add some config macro

--- zm_config.h
+++ zm_config.h
@@ -23,7 +23,16 @@
 extern "C"
 {
 #endif
+
+#define ZM_ENABLE    1
+#define ZM_DISABLE   0
+
+
     
+// <o> ZM_CLI_ARGC_MAX - Maximum number of parameters passed to the command handler. 
+#ifndef ZM_CLI_ARGC_MAX
+#define ZM_CLI_ARGC_MAX 12
+#endif
 
 // <o> ZM_CLI_CMD_BUFF_SIZE - Maximum buffer size for a single command. 
 #ifndef ZM_CLI_CMD_BUFF_SIZE
@@ -34,9 +43,40 @@ extern "C"
 #ifndef ZM_CLI_PRINTF_BUFF_SIZE
 #define ZM_CLI_PRINTF_BUFF_SIZE 23
 #endif
+// <q> ZM_FPRINTF_FLAG_AUTOMATIC_CR_ON_LF_ENABLED  - For each printed LF, function will add CR.
+#ifndef ZM_FPRINTF_FLAG_AUTOMATIC_CR_ON_LF_ENABLED
+#define ZM_FPRINTF_FLAG_AUTOMATIC_CR_ON_LF_ENABLED ZM_ENABLE
+#endif
 
-#define ZM_FPRINTF_FLAG_AUTOMATIC_CR_ON_LF
-    
+// <q> ZM_CLI_METAKEYS_ENABLED  - Enable additional control keys for CLI commands like ctrl+a, ctrl+e, ctrl+w, ctrl+u
+#ifndef ZM_CLI_METAKEYS_ENABLED
+#define ZM_CLI_METAKEYS_ENABLED ZM_ENABLE
+#endif
+
+// <e> ZM_CLI_HISTORY_ENABLED - Enable CLI history mode.
+//==========================================================
+#ifndef ZM_CLI_HISTORY_ENABLED
+#define ZM_CLI_HISTORY_ENABLED ZM_DISABLE
+#endif
+
+// <o> ZM_CLI_HISTORY_SAVE_ITEM_NUM - Number of history memory objects.
+#if ZM_CLI_HISTORY_ENABLED == ZM_ENABLE
+#ifndef ZM_CLI_HISTORY_ITEM_NUM
+#define ZM_CLI_HISTORY_SAVE_ITEM_NUM 30
+#endif
+#endif
+
+// <q> ZM_CLI_VT100_COLORS_ENABLED  - CLI VT100 colors.
+ 
+#ifndef ZM_CLI_VT100_COLORS_ENABLED
+#define ZM_CLI_VT100_COLORS_ENABLED ZM_ENABLE
+#endif
+
+// <q> ZM_CLI_WILDCARD_ENABLED  - Enable wildcard functionality for CLI commands.
+ 
+#ifndef ZM_CLI_WILDCARD_ENABLED
+#define ZM_CLI_WILDCARD_ENABLED ZM_DISABLE
+#endif
 
 #define ASSERT(expr)                                                          \
 do{                                                                           \
